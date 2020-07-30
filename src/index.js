9ee590c64a44fb6fb53aced34deb1037b76d8626
phanimkms1 <phanimkms1@gmail.com>
2020-07-30 23:38:05 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
+import Home from './screens/home/Home';
 ReactDOM.render(
-  <div></div>,
+  <Home/>,
   document.getElementById('root')
 );
 
