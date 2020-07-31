b6b79c3be42b8b2ece623a3f2264e027437820d3
phanimkms1 <phanimkms1@gmail.com>
2020-08-01 00:16:41 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,13 +5,35 @@ import logo from "../../assets/logo.svg";
 import Modal from "react-modal";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%,-50%)'
+    }
+}
+const TabContainer = function(props){
+    return(
+        <Typography component="div" style={{padding: 0}}>
+            {props.children}
+        </Typography>
+
+    );
+}
 class Header extends Component {
     constructor() {
         super();
         this.state = {
             modalIsOpen: false,
-            value:0
+            value: 0
         };
     }
     openModalHandler = () => {
@@ -20,20 +42,43 @@ class Header extends Component {
     closeModalHandler = () => {
         this.setState({ modalIsOpen: false })
     }
-    tabChangeHandler =(event,value) =>{
-        this.setState({value})
+    tabChangeHandler = (event, value) => {
+        this.setState({ value })
     }
     render() {
         return (
             <div className="app-header">
                 <img src={logo} className="app-logo" alt="logo" />
-                <Button className="login-button" variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+
+                <Button
+                    className="login-button"
+                    variant="contained"
+                    color="default"
+                    onClick={this.openModalHandler}>Login</Button>
+
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}>
+
                     <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login"></Tab>
                         <Tab label="Register"></Tab>
 
                     </Tabs>
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="username" >Username</InputLabel>
+                            <Input id="username" type="text"/>
+                        </FormControl>
+                        <FormControl required>
+                            <InputLabel htmlFor="password" >Password</InputLabel>
+                            <Input id="password" type="password"/>
+                        </FormControl>
+
+                    </TabContainer>
                 </Modal>
             </div>
 
