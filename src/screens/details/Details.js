3e562ab87b21ae0d08d75fea2a86ec29e30aa007
phanimkms1 <phanimkms1@gmail.com>
2020-08-02 17:44:19 +0530

    Added duration, release date, rating and plot on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,47 +1,60 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './Details.css';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import Typography from '@material-ui/core/Typography';
 
 class Details extends Component {
-    constructor(){
+    constructor() {
         super();
-            this.state={
-                movie:{}
-            }
-        
+        this.state = {
+            movie: {}
+        }
+
     }
 
-    componentWillMount(){
-        let currentState=this.state;
-        currentState.movie=moviesData.filter((mov)=>{
+    componentWillMount() {
+        let currentState = this.state;
+        currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
         })[0];
-        this.setState({currentState});
-        
+        this.setState({ currentState });
+
     }
-    render(){
+    render() {
 
-        let movie=this.state.movie;
+        let movie = this.state.movie;
 
-        return(<div className="details">
-            <Header/>
+        return (<div className="details">
+            <Header />
             <div className="flex-containerDetails">
                 <div className="leftDetails">
-                    <img src={movie.poster_url} alt={movie.title}/>
+                    <img src={movie.poster_url} alt={movie.title} />
                 </div>
                 <div className="middleDetails">
                     <div>
                         <Typography variant="h3" >{movie.title}</Typography>
                     </div>
+                    <br />
                     <div>
-                        <Typography><span className="bold">{movie.genres.join(",")}</span></Typography>
+                        <Typography><span className="bold">Genres: </span>{movie.genres.join(", ")}</Typography>
                     </div>
-                    
+                    <div>
+                        <Typography><span className="bold">Duration: </span> {movie.duration} </Typography>
+                    </div>
+                    <div>
+                        <Typography><span className="bold">Release Date: </span> {new Date(movie.release_date).toDateString()} </Typography>
+                    </div>
+                    <div>
+                        <Typography><span className="bold"> Rating: </span> {movie.critics_rating}  </Typography>
+                    </div>
+                    <div className="marginTop16">
+                        <Typography><span className="bold">Plot: </span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
+                    </div>
+
                 </div>
                 <div className="rightDetails">
-                    
+
                 </div>
             </div>
         </div>
